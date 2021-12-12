434a7e6278aac45983892d5fee0ac8442d478121
a-vengrzhinovskaya <94982772+a-vengrzhinovskaya@users.noreply.github.com>
2021-12-12 13:39:48 +0700

    Update Source.cpp

--- tetris/Source.cpp
+++ tetris/Source.cpp
@@ -1,60 +1,34 @@
 #include <SFML/Graphics.hpp>
-#include <ctime>
 
 using namespace sf;
 
-int field[10][20] = { 0 };
+const int fieldLenght = 20, fieldWidth = 10;
 
-struct tilePosition {
-	int x, y;
-};
-
-tilePosition a[4], b[4];
-
-int figure[7][4] = {
-	1,3,5,7,
-	2,4,5,7,
-	3,5,4,6,
-	3,5,4,7,
-	2,3,5,7,
-	3,5,7,6,
-	2,3,4,5,
-};
+int field[fieldLenght][fieldWidth];
 
 int main() {
 
-	int currentFigure;
+    RenderWindow window(VideoMode(320, 480), "Tetris");
 
-	srand(time(0));
+    while (window.isOpen()) {
 
-	RenderWindow window(VideoMode(320, 480), "Tetris");
+        window.setVerticalSyncEnabled(true);
 
-	Texture tTiles, tBackground;
-	tTiles.loadFromFile("C:/Users/aveng/source/repos/tetris/images/tiles.png");
-	tBackground.loadFromFile("C:/Users/aveng/source/repos/tetris/images/background.png");
+        Event event;
 
-	Sprite tiles(tTiles), background(tBackground);
+        while (window.pollEvent(event)) {
 
-	while (window.isOpen()) {
-		Event event;
-		while (window.pollEvent(event)) {
-			if (event.type == Event::Closed) {
-				window.close();
-			}
-		}
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
 
-		currentFigure = 6;
-		for (int tileNumber = 0; tileNumber < 4; ++tileNumber) {
-			a[tileNumber].x = figure[currentFigure][tileNumber] % 2;
-			a[tileNumber].y = figure[currentFigure][tileNumber] / 2;
-		}
+        window.clear();
 
-		window.clear();
-		window.draw(background);
-		for (int tileNumber = 0; tileNumber < 4; ++tileNumber) {
-			tiles.setPosition(a[tileNumber].x * 18, a[tileNumber].y * 18);
-			window.draw(tiles);
-		}
-		window.display();
-	}
+        
+
+        window.display();
+    }
+
+    return 0;
 }
