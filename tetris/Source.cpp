bc3e7f5e17224fda1ab28a7737a832900e6909a6
a-vengrzhinovskaya <94982772+a-vengrzhinovskaya@users.noreply.github.com>
2021-12-14 23:30:53 +0700

    Update Source.cpp

--- tetris/Source.cpp
+++ tetris/Source.cpp
@@ -5,7 +5,7 @@ using namespace sf;
 
 const int fieldLenght = 20, fieldWidth = 10, tileSize = 18;
 
-bool field[fieldLenght][fieldWidth];
+bool field[fieldLenght][fieldWidth] = {false};
 
 struct tilePosition {
     int x, y;
@@ -47,7 +47,7 @@ int main() {
 
     Sprite tile(tTiles), background(tBackground);
 
-    int figureType, numberOfTile;
+    int figureType, numberOfTile, direction = 0;
 
     while (window.isOpen()) {
 
@@ -59,25 +59,36 @@ int main() {
                 window.close();
             }
 
-            /*if (event.type == Event::KeyPressed) {
-                if (event.key.code == Keyboard::Up) {
+            if (event.type == Event::KeyPressed) {
+                /*if (event.key.code == Keyboard::Up) {
                     rotation();
-                }
-                else if (event.key.code == Keyboard::Right) {
-                    
+                }*/
+                if (event.key.code == Keyboard::Right) {
+                    direction = 1;
                 }
                 else if (event.key.code == Keyboard::Left) {
-                    
+                    direction = -1;
                 }
-            }*/
+            }
         }
 
-        figureType = rand() % 7;
+        if (tileCoords[0].y == 0) {
+
+            figureType = rand() % 7;
+
+            for (numberOfTile = 0; numberOfTile < 4; ++numberOfTile) {
+
+                tileCoords[numberOfTile].x = localTileCoords[figureType][numberOfTile] % 2;
+                tileCoords[numberOfTile].y = localTileCoords[figureType][numberOfTile] / 2;
+            }
+        }
+
         for (numberOfTile = 0; numberOfTile < 4; ++numberOfTile) {
 
-            tileCoords[numberOfTile].x = localTileCoords[figureType][numberOfTile] % 2;
-            tileCoords[numberOfTile].y = localTileCoords[figureType][numberOfTile] / 2;
+            tileCoords[numberOfTile].x += direction;
         }
+
+        direction = 0;
 
         window.clear(Color::White);
 
